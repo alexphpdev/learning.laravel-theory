06a2bad809ceb6e4476ca26feca44fb23ab3a235
alexphpdev <work.forfood@mail.ru>
2018-04-08 17:38:18 +0300

    Именованные маршруты. Регулярные выражения для параметров адресной строки. Необязательные параметры. Group(prefix). Редирект функцией route (так же и с параметрами). Получение объекта Route. Связка redirect и route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', ['as' => 'home', function () {
      return view('welcome');
-});
+}]);
 
 Route::get('/page', function () {
     return view('page');
@@ -44,3 +44,71 @@ Route::get('/form', function () {
 Route::any('/comments', function () {
     print_r($_POST);
 });
+
+
+//Route::get('/page/{id}/{cat?}', function ($idd, $catt=null) {
+//    print_r($idd);
+//    if(isset($catt)) {
+//        print_r($catt);
+//    }
+//
+//});
+
+Route::get('/page/{id}', function ($id) {
+   print_r($id);
+})->where('id', '[0-9]+');
+
+Route::get('/page/{id}/{cat}', function ($id, $cat) {
+   print_r($id);
+   echo "<br>";
+   print_r($cat);
+})/*->where([
+    'id'=> '[0-9]+',
+    'cat' => '[a-zA-Z]+',
+])*/;
+
+Route::get('/article/{id}', ['as' => 'article', function ($id) { echo "/article/$id";}]);
+
+
+Route::group(['prefix' => 'admin/{id}'], function () {
+
+    Route::get('/page/create', function () { echo '/page/create';});
+
+    Route::get('/page/edit', function () { echo '/page/edit';});
+
+    Route::get('/page/links', function () {
+        echo "<a href='".route('home')."'>";
+        echo 'Go home';
+        echo '</a>';
+
+        echo '<br>';
+
+        echo "<a href='".route('article', ['id' => 10])."'>";
+        echo 'Go to article with ID = 10';
+        echo '</a>';
+    });
+
+    Route::get('/page/getParametersFromObject/{bla}', function () {
+        $route = Route::current();
+        echo $route->getName();
+        echo '<br>';
+        print_r($route->parameters());
+    })->name('route object');
+
+
+
+    Route::get('/page/redirectToHome', function () {
+        return redirect()->route('home');
+    });
+
+
+});
+
+
+
+
+
+
+
+
+

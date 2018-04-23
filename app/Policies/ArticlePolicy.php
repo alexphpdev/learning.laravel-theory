29fd06ae5a5b9b7983bd2ecca811e5b90fe1b091
alexphpdev <work.forfood@mail.ru>
2018-04-23 19:09:41 +0300

    policy

---
+++ app/Policies/ArticlePolicy.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Article;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArticlePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function add(User $user)
+    {
+        foreach ($user->roles as $role) {
+            if (strtolower($role->name) == 'admin') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function update(User $user, Article $article)
+    {
+        foreach ($user->roles as $role) {
+            if (strtolower($role->name) == 'admin') {
+                if ($user->id == $article->user_id) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
